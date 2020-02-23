75a3fde3dfbe2637bdbee873166dafe285bfe848
saki918 <hiro9181013@gmail.com>
2020-02-23 13:37:36 +0900

    メンバー コントローラ ビュー 内 記述

--- app/controllers/guest/members_controller.rb
+++ app/controllers/guest/members_controller.rb
@@ -1,4 +1,38 @@
 class Guest::MembersController < ApplicationController
+  before_action :current_guest
+  
   def show
+    @member = Member.find(params[:id])
+    @events = @member.events
+  end
+
+  def create
+    @new_member = current_guest.member.new(member_params)
+    # @new_member.guest_id == current_guest.id
+    if @new_member.save
+      redirect_to members_show_path
+    else
+      render guests_show_path
+    end
+  end
+
+  def update
+    @member = Member.find(params[:id])
+    if @member.update(member_params)
+      redirect_to request.referer
+    else
+      render guests_show_path
+    end
+  end
+
+  def destroy
+    member = Member.find(params[:id])
+    member.destroy
+    redirect_to guests_show_path
+  end
+
+private
+  def member_params
+    params.require(:member).permit(:name)
   end
 end
