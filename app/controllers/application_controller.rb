07c83010f6e1cd21532654ef7b0e22d675f391d0
saki918 <hiro9181013@gmail.com>
2020-02-23 13:35:32 +0900

    ログイン、ログアウト後のURL設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,25 @@
 class ApplicationController < ActionController::Base
-  before_action :configure_permitted_parameters, if: :devise_controller?
+  #デバイス機能実行前にconfigure_permitted_parametersの実行をする。
+  # before_action :configure_permitted_parameters, if: :devise_controller?
+  # CSRF対策(クロスサイトリクエストフォージェリ)
+  # サイトに攻撃用のコードを仕込むことで、アクセスしたユーザーに対して意図しない操作を行わせる
+  # 攻撃を防ぐ
+  protect_from_forgery with: :exception
+protected
+  def after_sign_in_path_for(resource)
+    case resource
+    when Guest
+      events_path
+    when Admin
+      admin_guests_path
+    end
+  end
+  def after_sign_out_path_for(resource)
+    case resource
+    when Guest
+      root_path
+    when Admin
+      new_admin_session_path
+    end
+  end
 end
