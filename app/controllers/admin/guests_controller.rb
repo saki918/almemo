873ea68d793698c79a6b98ee5aa09b154f6d52fb
saki918 <hiro9181013@gmail.com>
2020-02-23 13:33:07 +0900

    ゲスト コントローラ ビュー 作成

--- app/controllers/admin/guests_controller.rb
+++ app/controllers/admin/guests_controller.rb
@@ -1,10 +1,35 @@
 class Admin::GuestsController < ApplicationController
+  before_action :current_admin
+
+  def index
+  end
+
   def show
+    @guest = Guest.find(params[:id])
+    @members = @guest.members
   end
 
   def edit
+    @guest = Guest.find(params[:id])
   end
 
-  def index
+  def update
+    @guest = Guest.find(params[:id])
+    if @guest.save
+      redirect_to @guest
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @guest = Guest.find(params[:id])
+    @guest.destroy
+    redirect_to admin_guests_path
+  end
+
+  private
+  def guest_params
+    params.require(:guest).permit(:name)
   end
 end
--- app/controllers/guest/guests_controller.rb
+++ app/controllers/guest/guests_controller.rb
@@ -1,7 +1,20 @@
 class Guest::GuestsController < ApplicationController
+  before_action :current_guest
+
   def show
   end
 
   def edit
+  end
+  def update
+    if current_guest.save(guest_params)
+      redirect_to current_guest
+    else
+      render current_guest
+    end
+  end
+  private
+  def guest_params
+    params.require(:guest).permit(:name,:email)
   end
 end
