ddf25a2499830b38d5c624bfd4a2350e02520d28
saki918 <hiro9181013@gmail.com>
2020-02-26 17:54:00 +0900

    モデル 記述 修正

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,12 @@
 class Event < ApplicationRecord
   belongs_to :guest
-  belongs_to :genre
   has_many :event_members
+  has_many :images
+  has_many :members
   attachment :image, destroy: false
+  # accepts_nested_attributes_forを使うことで簡単に1対多のモデルを一度に更新するフォームを作成することできます。
+  accepts_nested_attributes_for :event_members
+  accepts_nested_attributes_for :images
+
+  enum genre_id: [:meal, :date, :work, :sport, :shopping, :drive, :drinking_party, :movie, :celebration]
 end
