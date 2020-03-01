73a473bc1a032c2cf2c1a4656a07289bd2f7ca84
saki918 <hiro9181013@gmail.com>
2020-03-01 13:26:52 +0900

    リレーション バリデーション carrierwave用の記述

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,7 +1,7 @@
 class Member < ApplicationRecord
   belongs_to :guest
   has_many :event_members
-  has_many :events
+  has_many :events,through: :event_members
 
   validates :name, presence: true
 end
