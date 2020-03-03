b96229607d4ccd2ba855c825d1051d2c5ee758e7
saki918 <hiro9181013@gmail.com>
2020-03-03 10:37:45 +0900

    ジャンル 編集フォーム作成 レイアウト変更

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -7,13 +7,25 @@ class Admin::GenresController < ApplicationController
     @genres = Genre.all
   end
 
+  def edit
+    @genre = Genre.find(params[:id])
+  end
   def create
     @new_genre = Genre.new(genre_params)
     if @new_genre.save
       flash[:notice] = 'ジャンルを作成しました'
       redirect_to admin_genres_path
     else
-      render :new
+      render 'index'
+    end
+  end
+  def update
+    @genre = Genre.find(params[:id])
+    if @genre.update(genre_params)
+      flash[:notice] = 'ジャンルの編集に成功しました'
+      redirect_to admin_genres_path
+    else
+      render 'edit'
     end
   end
   def destroy
