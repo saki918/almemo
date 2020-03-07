e3e367486e31c87c0fac79aa8bb71b0a7f146ced
saki918 <hiro9181013@gmail.com>
2020-03-07 14:50:28 +0900

    javascriptが二回呼び出される為、rails-ujsを削除

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,10 +10,9 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require rails-ujs
 //= require activestorage
 //= require jquery
-// = require jquery_ujs
+//= require jquery_ujs
 //= require multiple-select.min
 //= require bootstrap-sprockets
 
