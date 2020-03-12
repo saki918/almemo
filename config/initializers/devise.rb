dbb89e7dbfc73610f994e2c00c5b7ad9f22a2d31
saki918 <hiro9181013@gmail.com>
2020-03-12 20:12:28 +0900

    セッションハイジャック 設定

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -176,7 +176,8 @@ Devise.setup do |config|
   # ==> Configuration for :timeoutable
   # The time you want to timeout the user session without activity. After this
   # time the user will be asked for credentials again. Default is 30 minutes.
-  # config.timeout_in = 30.minutes
+  # ユーザーのセッションを30分で切れるように設定。
+  config.timeout_in = 30.minutes
 
   # ==> Configuration for :lockable
   # Defines which strategy will be used to lock an account.
