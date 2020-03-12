69307fde54c0414c4e79c9307a2321ab357e371f
saki918 <hiro9181013@gmail.com>
2020-03-12 20:12:28 +0900

    セッションハイジャック 設定

--- app/models/guest.rb
+++ app/models/guest.rb
@@ -2,9 +2,10 @@ class Guest < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+        :recoverable, :rememberable, :validatable
   has_many :members
   has_many :events
   has_many :images
-  
+  # ユーザーのセッションを30分で切れるようにする為の設定
+  devise :timeoutable
 end
