9db2e059e0b1e4c170fcd5ab03492103d9261b7c
saki918 <hiro9181013@gmail.com>
2020-03-14 21:45:06 +0900

    コーディング修正

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 class Member < ApplicationRecord
   belongs_to :guest
   has_many :event_members
-  has_many :events,through: :event_members
+  has_many :events, through: :event_members
 
   validates :name, presence: true
 end
