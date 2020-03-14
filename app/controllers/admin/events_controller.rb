68dc95f69f8bb5618d60c6ec4cccf37aa505a9d2
saki918 <hiro9181013@gmail.com>
2020-03-15 04:07:21 +0900

    リファクタリング

--- app/controllers/admin/events_controller.rb
+++ app/controllers/admin/events_controller.rb
@@ -1,21 +1,7 @@
+# frozen_string_literal: true
+
 class Admin::EventsController < ApplicationController
   before_action :current_admin
-  def show
-    @event = Event.find(params[:id])
-  end
-
-  def edit
-    @event = Event.find(params[:id])
-  end
-
-  def update
-    @event = Event.find(params[:id])
-    if @event.update(event_params)
-      redirect_to @event
-    else
-      render 'edit'
-    end
-  end
 
   def destroy
     event = Event.find(params[:id])
